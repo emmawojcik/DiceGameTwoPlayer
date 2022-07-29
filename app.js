bb5077f402ae1a926e7717cf09be6baa5daf6ac2
emmawojcik <71179439+emmawojcik@users.noreply.github.com>
2022-07-29 12:21:04 +0100

    majority of game is functioning

--- app.js
+++ app.js
@@ -5,15 +5,15 @@ const rollBtn = document.getElementById('rollBtn');
 const holdBtn = document.getElementById('holdBtn');
 const playerOneCurrentLabel = document.getElementById('playerOneCurrentLabel');
 const playerTwoCurrentLabel = document.getElementById('playerTwoCurrentLabel');
-const playerOneScoreLabel = document.getElementById('playerOneScoreLabel');
-const playerTwoScoreLabel = document.getElementById('playerTwoScoreLabel');
+const playerOneHoldScoreLabel = document.getElementById('playerOneScoreLabel');
+const playerTwoHoldScoreLabel = document.getElementById('playerTwoScoreLabel');
 const playerOneMainLabel = document.getElementById('playerOneMainLabel');
 const playerTwoMainLabel = document.getElementById('playerTwoMainLabel');
 
-
 let currentPlayer = "playerOne";
 let currentScore = 0;
-let holdScore = 0;
+let playerOneHoldScore = 0;
+let playerTwoHoldScore = 0;
 
 function rollDice(){
   return Math.ceil(Math.random() * 6);
@@ -27,43 +27,65 @@ function changeCurrentPlayer() {
   }
 }
 
+function playerRoll(diceRollTurn) {
+  if(currentPlayer == "playerOne"){
+    if(diceRollTurn == 1 && playerOneHoldScore == 0){
+      currentScore = 0;
+      playerOneCurrentLabel.textContent = `Current: ${currentScore}`;
+      changeCurrentPlayer();
+    } else {
+      currentScore += diceRollTurn;
+      playerOneCurrentLabel.textContent = `Current: ${currentScore}`;
+    }
+  } else {
+      if(diceRollTurn == 1 && playerTwoHoldScore == 0){
+        currentScore = 0;
+        playerTwoCurrentLabel.textContent = `Current: ${currentScore}`;
+        changeCurrentPlayer();
+      } else {
+        currentScore += diceRollTurn;
+        playerTwoCurrentLabel.textContent = `Current: ${currentScore}`;
+      }
+  }
+}
+
+function playerHold(){
+  if(currentPlayer == "playerOne"){
+    playerOneHoldScore += currentScore;
+    playerOneHoldScoreLabel.textContent = playerOneHoldScore;
+    if(playerOneHoldScore >= 20){
+      playerOneMainLabel.textContent = "Winner!";
+      playerOneHoldScore = 0;
+    } 
+    currentScore = 0;
+    playerOneCurrentLabel.textContent = `Current: ${currentScore}`;
+
+  } else {
+    playerTwoHoldScore += currentScore;
+    playerTwoHoldScoreLabel.textContent = playerTwoHoldScore;
+    if(playerTwoHoldScore >= 20){
+      playerTwoMainLabel.textContent = "Winner!";
+      playerTwoHoldScore = 0;
+    }
+    currentScore = 0;
+    playerTwoCurrentLabel.textContent = `Current: ${currentScore}`;
+  }
+}
+
 newGameBtn.addEventListener('click', () => {
   playBtns.style.visibility = 'visible';
+  currentPlayer = "playerOne";
 })
 
 rollBtn.addEventListener('click', () => {
   let diceRollTurn = rollDice();
   diceImage.src = `images/dice${diceRollTurn}.png`;
   diceImage.style.visibility = 'visible';
-
-  if(diceRollTurn == 1 && holdScore == 0){
-    currentScore = 0;
-    changeCurrentPlayer();
-  } else {
-    currentScore += diceRollTurn;
-    if(currentPlayer == "playerOne"){
-      playerOneCurrentLabel.textContent = currentScore;
-    } else {
-      playerTwoCurrentLabel.textContent = currentScore;
-    }
-  }
-
+  playerRoll(diceRollTurn);
 })
 
 holdBtn.addEventListener('click', () => {
-  holdScore += currentScore;
-  if(holdScore >= 20){
-    if(currentPlayer == "playerOne"){
-      playerOneMainLabel.textContent = "Winner!";
-    } else {
-      playerTwoMainLabel. textContent = "Winner!";
-    }
-  }
-  if(currentPlayer == "playerOne"){
-    playerOneScoreLabel.textContent = holdScore;
-  } else {
-    playerTwoScoreLabel.textContent = holdScore;
-  }
+  playerHold();
   currentScore = 0;
   changeCurrentPlayer();
 })
